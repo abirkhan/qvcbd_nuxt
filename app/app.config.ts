160820c73124969cc36bca05ff6d41ddee5f9941
Abir <abirkhan@outlook.com>
2025-09-17 03:45:41 -0700

    F2

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
   global: {
     picture: {
-      dark: 'https://scontent.fspd3-1.fna.fbcdn.net/v/t39.30808-6/303549191_208561308170394_5745016480458344863_n.png?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=pmD-eNqYlKYQ7kNvwHa7eDH&_nc_oc=AdkEbXx6oRxqdhT4LDUx7IHhTFcUmHh7lJxqeNJgqN967S-cjV5-qNlNZxe_4yglhYQ&_nc_zt=23&_nc_ht=scontent.fspd3-1.fna&_nc_gid=UFtdjggC1yST1emCbc4Z5A&oh=00_AfbPcEEYRrYb984G2ts3odT7iYJ1uGNHtqrG2upTCVCmqw&oe=68CC46C6',
-      light: 'https://scontent.fspd3-1.fna.fbcdn.net/v/t39.30808-6/303549191_208561308170394_5745016480458344863_n.png?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=pmD-eNqYlKYQ7kNvwHa7eDH&_nc_oc=AdkEbXx6oRxqdhT4LDUx7IHhTFcUmHh7lJxqeNJgqN967S-cjV5-qNlNZxe_4yglhYQ&_nc_zt=23&_nc_ht=scontent.fspd3-1.fna&_nc_gid=UFtdjggC1yST1emCbc4Z5A&oh=00_AfbPcEEYRrYb984G2ts3odT7iYJ1uGNHtqrG2upTCVCmqw&oe=68CC46C6',
+      dark: '/qvc-logo.png',
+      light: '/qvc-logo.png',
       alt: 'QVC BD Limited'
     },
     meetingLink: '',
